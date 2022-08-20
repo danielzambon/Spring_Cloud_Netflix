dac19a3f546812ed04e3448b0e62c413817b9d84
danielzambon <danielfzambon@gmail.com>
2022-08-19 21:08:18 -0300

    FindAll

--- src/main/java/com/DanielZambon/crud/controller/ProdutoController.java
+++ src/main/java/com/DanielZambon/crud/controller/ProdutoController.java
@@ -1,5 +1,41 @@
 package com.DanielZambon.crud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.DanielZambon.crud.data.vo.ProdutoVO;
+import com.DanielZambon.crud.services.ProdutoService;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("/produto")
 public class ProdutoController {
+	
+	private final ProdutoService produtoService;
+	private final PagedResourcesAssembler<ProdutoVO> assembler;
+	
+	@Autowired
+	public ProdutoController(ProdutoService produtoService, PagedResourcesAssembler<ProdutoVO> assembler) {
+		super();
+		this.produtoService = produtoService;
+		this.assembler = assembler;
+	}
+	
+	@GetMapping(value = "/{id}", produces= {"application/json","application/xml","application/x-yaml"})
+	public ProdutoVO findById(@PathVariable("id") Long id) {
+		
+		ProdutoVO produtoVO = produtoService.findById(id);
+		produtoVO.add(linkTo(methodOn(ProdutoController.class).findById(id)).withSelfRel());
+		
+		return null;
+	}
+	
+	
 
 }
