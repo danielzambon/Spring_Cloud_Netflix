ac491a655ecff33408c454cf0b710a5ec61d54f6
danielzambon <danielfzambon@gmail.com>
2022-08-19 21:15:20 -0300

    Post create

--- src/main/java/com/DanielZambon/crud/controller/ProdutoController.java
+++ src/main/java/com/DanielZambon/crud/controller/ProdutoController.java
@@ -1,32 +1,48 @@
 package com.DanielZambon.crud.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.EntityMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.DanielZambon.crud.data.vo.ProdutoVO;
+import com.DanielZambon.crud.entity.Produto;
 import com.DanielZambon.crud.services.ProdutoService;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
-	
+
 	private final ProdutoService produtoService;
 	private final PagedResourcesAssembler<ProdutoVO> assembler;
-	
+
 	@Autowired
 	public ProdutoController(ProdutoService produtoService, PagedResourcesAssembler<ProdutoVO> assembler) {
 		super();
 		this.produtoService = produtoService;
 		this.assembler = assembler;
 	}
-	
+
 	@GetMapping(value = "/{id}", produces= {"application/json","application/xml","application/x-yaml"})
 	public ProdutoVO findById(@PathVariable("id") Long id) {
 		
@@ -35,7 +51,30 @@ public class ProdutoController {
 		
 		return null;
 	}
+
+	@GetMapping(produces= {"application/json","application/xml","application/x-yaml"})
+	public ResponseEntity<?> findAll(
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "12") int limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection,"nome"));
+		Page<ProdutoVO> produtos = produtoService.findAll(pageable);
+		produtos.stream().forEach(p -> p.add(linkTo(methodOn(ProdutoController.class).findById(p.getId())).withSelfRel()));
+		PagedModel<EntityModel<ProdutoVO>> pageModel = assembler.toModel(produtos);
+		
+		return new ResponseEntity<>(pageable,HttpStatus.OK);
+	}
 	
+	@PostMapping(produces= {"application/json","application/xml","application/x-yaml"}, consumes = {"application/json","application/xml","application/x-yaml"})
+	public ProdutoVO create(@RequestBody ProdutoVO produtoVO) {
+		
+		ProdutoVO proVO = produtoService.create(produtoVO);
+		proVO.add(linkTo(methodOn(ProdutoController.class).findById(produtoVO.getId())).withSelfRel());
+		return proVO;
+		
+	}
 	
 
 }
